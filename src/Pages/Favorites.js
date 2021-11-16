c7cb6dc11228e9375c3039ab8283e02eeec8a059
chaimakhorchid <khorchid.chaima@hotmail.fr>
2021-11-16 17:21:17 +0100

    'modif'

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
 
 export default class Favorites extends Component {
+    constructor() {
+        super ()
+
+        this.state= {
+            movies: [],
+            favIDs: this.getStorage()
+        
+        }
+
+        this.getStorage=this.getStorage.bind(this)
+        this.getMovie=this.getMovie.bind(this)
+    }
+
+componentDidMount() {
+    this.state.favIDs.forEach(favId => (
+        this.getMovie(favId)
+    ));
+}
+
+getStorage() {
+    const id = localStorage.getItem("favorites")
+    return (
+        JSON.parse(id)
+    )
+}
+
+
+getMovie(id){
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=74ff4d5b18f55c304a239fadf716fe2f`)
+    .then(result => result.json())
+    .then(data => {
+        this.setState({movies:[...this.state.movies, data]})
+    })
+}
+
+
+
     render() {
-        return (
-            <div>
-            <h1>Favorites</h1>    
-            </div>
+        return (           
+            <div className="container">
+            <h1>Favorites</h1> 
+            <div className="row">
+            {this.state.movies.map((movie) => {
+                        return (
+                            <div className="col-4  my-3"  key={movie.id}>
+                                <div className="m-y h-100">
+                                    <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>
+                                    <h1>{movie.title}</h1>
+                                    <p>{movie.release_date}</p>
+                                    <p>{movie.overview}</p>
+                                </div>
+                            </div>
+                            ) 
+                        })}   
+             </div>
+             </div>
         )
     }
 }
--- src/Pages/PopularBattle.js
+++ src/Pages/PopularBattle.js
@@ -1,42 +1,84 @@
 import React, { Component } from 'react'
-import Card from '../Components/Card'
+// import Card from '../Components/Card'
 
 export default class Popular extends Component {
     constructor (){
         super()
-        this.state= {
-            movies: []
+        this.state = {
+            movies: [],
+            currentBattle : 0,
+            favorites: []
         }
+        this.handleMovieChange = this.handleMovieChange.bind(this)
     }
+
     componentDidMount() {
     
         fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=74ff4d5b18f55c304a239fadf716fe2f`)
             .then(response => response.json())
             .then(data => {
                 this.setState({ movies: data.results })
+               
             })
     }
 
+    handleMovieChange(id){
+
+        const {favorites, currentBattle} = this.state
+
+        if(currentBattle === 18){
+            alert("Vous avez parcouru tous les films !")
+        }
+
+        // console.log(id);
+        this.setState({
+            currentBattle : this.state.currentBattle += 2,
+            favorites: [id,...favorites]
+
+        })
+        // this.setState({
+        //     currentBattle : this.state.currentBattle += 2
+        //     favoritesArray = [...favoritesArray,id]
+        // })
+        // const favoritesArray= localStorage.setItem("favorites")
+        // if(!favoritesArray){
+        //     favoritesArray = localStorage.setItem("favorites", JSON.stringify([id]))
+        // } else {
+        //     favoritesArray = JSON.parse(favoritesArray)
+        //     favoritesArray = [...favoritesArray,id]
+        //     favoritesArray = localStorage.setItem("favorites", JSON.stringify(favoritesArray))
+        // }
+    }
 
     render() {
-        console.log(this.state.movies);
+        // console.log(this.state.movies);
+        const filterMovies = this.state.movies.filter((movies, index) =>
+         index === this.state.currentBattle || index === this.state.currentBattle + 1)
+         localStorage.setItem("favorites",JSON.stringify(this.state.favorites))
+        // console.log(this.state.favorites);
+        console.log(this.state.currentBattle);
+        
         return (
             <div >
                 <h1>Popular Battle</h1>
                 <div className="d-flex flex-wrap">
-                    {this.state.movies.map(function(movie) {
-                        return <Card
-                            original_title={movie.original_title}
-                            release_date={movie.release_date}
-                            overview={movie.overview}
-                            poster_path={movie.poster_path}
-                            key={movie.id}
-                            handleClick={this.handleMovie}
-                        />
+                    {filterMovies.map((movie) => {
+                        return (
+                            <div className="card m-3" style={{width: "18rem"}} onClick={() => this.handleMovieChange(movie.id)} key={movie.id}>
+                                <div style={{height: "14rem", overflow: "hidden"}}>
+                                    <img src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>
+                                </div>
+                                <div className="card-body">
+                                    <h1>{movie.title}</h1>
+                                    <p>{movie.release_date}</p>
+                                    <p>{movie.overview}</p>
+                                
+                                </div>
+                            </div>
+                                )
                     })}               
                 </div>
             </div>
         )
     }
 }
-
